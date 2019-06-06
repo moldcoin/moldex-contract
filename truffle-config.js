bbef05898009fa83d878b37572b76e33aac8208b
EG-easy <1018eguchi@gmail.com>
2019-06-06 18:36:13 +0900

    set environment variables

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,7 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
-const infuraKey = "1a6eee29ae624e269a93820d108608c6";
-const mnemonic = "regular fury alone matrix skull decide sound front trip profit gown lucky";
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraKey = process.env.INFURA_KEY;
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
@@ -15,6 +14,14 @@ module.exports = {
 			provider: () => new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
       network_id: 3,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    main: {
+			provider: () => new HDWalletProvider(mnemonic, `https://mainnet.infura.io/v3/${infuraKey}`),
+      network_id: 1,       // Mainnet's id
+      gas: 10000000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
